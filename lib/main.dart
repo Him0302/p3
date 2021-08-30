e18f98603016b31ea0035340ca11d32adfc02c43
Him0302 <81606642+Him0302@users.noreply.github.com>
2021-08-31 02:25:27 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,35 +26,64 @@ class layout extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text('Analytics'),
         Container(
-          //color: Colors.white,
+          padding: EdgeInsets.all(4.0),
+          alignment: AlignmentDirectional.topStart,
+          child: Text(
+            'Analytics',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Container(
           padding: EdgeInsets.all(10.0),
           margin: EdgeInsets.all(10.0),
-          height: 220.0,
+          height: 190.0,
           width: 400.0,
           decoration: BoxDecoration(
               borderRadius: BorderRadius.all(Radius.circular(10.0)),
               color: Colors.white),
           child: Row(children: [
-            Text('Sales'),
+            Container(
+                height: 200.0,
+                child: Text(
+                  'Sales',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
             SizedBox(
               width: 2.0,
             ),
-            Icon(Icons.arrow_drop_down),
+            Container(
+                alignment: AlignmentDirectional.topStart,
+                height: 200.0,
+                child: Icon(Icons.arrow_drop_down)),
             SizedBox(width: 150.0),
-            Text(
-              'Select date',
-              style: TextStyle(
-                color: Colors.blue,
+            Container(
+              height: 200.0,
+              child: Text(
+                'Select date',
+                style: TextStyle(
+                  color: Colors.blue[600],
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
             SizedBox(
               width: 2.0,
             ),
-            Icon(
-              Icons.arrow_drop_down,
-              color: Colors.blue,
+            Container(
+              alignment: AlignmentDirectional.topStart,
+              height: 200.0,
+              child: Icon(
+                Icons.arrow_drop_down,
+                color: Colors.blue,
+              ),
             ),
           ]),
         ),
@@ -70,16 +99,20 @@ class layout extends StatelessWidget {
               width: 150.0,
               child: Column(
                 children: [
-                  Icon(Icons.account_circle_sharp,
-                      color: Colors.grey, size: 70.0),
+                  Container(
+                    child: Icon(Icons.account_circle_sharp,
+                        color: Colors.grey[600], size: 70.0),
+                  ),
                   SizedBox(
                     height: 2.0,
                   ),
-                  Text(
-                    'Customer',
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 20.0,
+                  Container(
+                    child: Text(
+                      'Customer',
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontSize: 20.0,
+                      ),
                     ),
                   ),
                   SizedBox(
@@ -87,17 +120,26 @@ class layout extends StatelessWidget {
                   ),
                   Row(
                     children: [
-                      Text(
-                        'Total',
-                        style: TextStyle(
-                          fontSize: 18.0,
+                      Container(
+                        width: 80.0,
+                        alignment: AlignmentDirectional.bottomCenter,
+                        child: Text(
+                          'Total',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.grey[600],
+                          ),
                         ),
                       ),
-                      SizedBox(width: 60.0),
-                      Text(
-                        '78',
-                        style: TextStyle(
-                          fontSize: 18.0,
+                      //SizedBox(width: 30.0),
+                      Container(
+                        width: 40.0,
+                        child: Text(
+                          '78',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.grey[600],
+                          ),
                         ),
                       )
                     ],
@@ -145,9 +187,11 @@ class layout extends StatelessWidget {
                       width: 140.0,
                     ),
                     Container(
+                      width: 70.0,
+                      alignment: AlignmentDirectional.bottomCenter,
                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(4.0)),
-                          color: Colors.green),
+                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                          color: Colors.green[400]),
                       child: Text(
                         'View all',
                         style: TextStyle(
@@ -162,71 +206,104 @@ class layout extends StatelessWidget {
               Row(
                 children: [
                   Container(
+                    height: 180.0,
+                    alignment: AlignmentDirectional.topStart,
                     width: 90.0,
-                    child: Image(
-                      image: AssetImage('images/iphone.png'),
+                    child: Container(
+                      child: Image(
+                        image: AssetImage('images/iphone.png'),
+                      ),
                     ),
                   ),
                   Container(
                     child: Column(
                       children: [
-                        Text('Apple Iphone 12'),
+                        Container(
+                            width: 220.0,
+                            child: Text(
+                              'Apple Iphone 12',
+                              style: TextStyle(
+                                fontSize: 16.0,
+                              ),
+                            )),
                         SizedBox(
                           height: 2.0,
                         ),
-                        Row(
-                          children: [
-                            Icon(Icons.star, color: Colors.green),
-                            SizedBox(width: 3.0),
-                            Text('4.5',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                ))
-                          ],
-                        ),
-                        SizedBox(height: 2.0),
-                        Text(
-                          'Very Nice',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
+                        Container(
+                          width: 220.0,
+                          child: Row(
+                            children: [
+                              Icon(
+                                 Icons.star,
+                                size: 20.0,
+                                color: Colors.green[400],
+                              ),
+                              SizedBox(width: 8.0),
+                              Text('4.5',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15.0,
+                                  ))
+                            ],
                           ),
                         ),
-                        Text('Good quality as described'),
+                        SizedBox(height: 2.0),
+                        Container(
+                          width: 220.0,
+                          child: Text(
+                            'Very Nice',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Container(
+                            width: 220.0,
+                            child: Text('Good quality as described')),
                         Row(
                           children: [
                             Container(
                               height: 90.0,
+                              width: 110.0,
                               decoration: BoxDecoration(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(8.0))),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(4.0)),
+                              ),
                               child: Image(
-                                image: AssetImage('images/Rectangle 8.png'),
+                                image: AssetImage(
+                                  'images/Rectangle 8.png',
+                                ),
                               ),
                             ),
-                            SizedBox(
-                              width: 5.0,
-                            ),
                             Container(
+                              width: 120.0,
                               height: 90.0,
                               decoration: BoxDecoration(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(8.0))),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(4.0)),
+                              ),
                               child: Image(
                                 image: AssetImage('images/Rectangle 8.png'),
                               ),
                             )
                           ],
                         ),
-                        Text(
-                          'Gaurav Sharma,delhi',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
+                        Container(
+                          width: 220.0,
+                          child: Text(
+                            'Gaurav Sharma,delhi',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
-                        Text(
-                          'Feb ,2020',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
+                        Container(
+                          width: 220.0,
+                          child: Text(
+                            'Feb ,2020',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         )
                       ],
